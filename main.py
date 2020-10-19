ee7be71d71d1152087b9b9405bd59ac342021074
DESKTOP-Q5F3DE1\cwozn <Cwozniczka@outsetmedical.com>
2020-10-19 16:01:36 -0500

    Any JPG in the Photo file will now have a watermarked version the Hotdog Analysis folder. This is done by the new watermark function

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tensorflow.keras
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 import numpy as np
 import os
+
+#creates a new image with a Hotdog or Not Hotdog water mark in the Hotdog Analysis folder
+def watermark(analysis):
+    print(f'File: {filename} is a Hot Dog')
+    width, height = image.size
+    output = ImageDraw.Draw(image)
+    font = ImageFont.truetype('arial.ttf', 42)
+    textwidth, textheight = output.textsize(analysis, font)
+    margin = 0
+    x = width - textwidth - margin
+    y = height - textheight - margin
+    output.text((x, y), analysis, font=font)
+    image.save(f'./Hotdog Analysis/{filename}')
+
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
@@ -19,16 +33,16 @@ for filename in os.listdir('./Photo'):
     if filename.endswith('.jpg'):
         image = Image.open(str('./Photo/' + filename))
 
-        #resize the image to a 224x224 with the same strategy as in TM2:
-        #resizing the image to be at least 224x224 and then cropping from the center
+        # resize the image to a 224x224 with the same strategy as in TM2:
+        # resizing the image to be at least 224x224 and then cropping from the center
         size = (224, 224)
         image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
-        #turn the image into a numpy array
+        # turn the image into a numpy array
         image_array = np.asarray(image)
 
         # display the resized image
-        image.show()
+        # image.show()
 
         # Normalize the image
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
@@ -39,10 +53,13 @@ for filename in os.listdir('./Photo'):
         # run the inference
         prediction = model.predict(data)
         hotdog_percent = prediction.item(0) * 100
-        print(f'file name: {filename} Percent Hot Dog: {hotdog_percent}')
         if hotdog_percent > 90:
             print(f'File: {filename} is a Hot Dog')
+            analysis = 'HotDog'
+            watermark(analysis)
         else:
             print(f'File: {filename} is not a Hot Dog')
+            analysis = 'Not HotDog'
+            watermark(analysis)
     else:
         print('JPG files only please')
